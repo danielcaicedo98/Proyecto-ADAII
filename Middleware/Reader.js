89871949b76cbc27ba02fdb7d912f09438ca42de
danielcaicedo98 <daniel.andres.caicedo@correounivalle.edu.co>
2023-09-17 18:50:54 -0500

    Método que permite leer el archivo y retorna un JSON con los datos

---
+++ Middleware/Reader.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const fs = require('fs');
+
+function readData() {
+  try {
+    const filePath = path.join(__dirname,  'input.txt'); // Ruta relativa a input.txt
+    const contenido = fs.readFileSync(filePath, 'utf-8');
+    const contenido_l = contenido.split('\n');
+    const numero_materias = parseInt(contenido_l[0].trim());
+
+    // Objeto JSON para almacenar las materias
+    const materiasJSON = {
+      numero_materias: numero_materias,
+      materias: []
+    };
+
+    // Organiza las materias que están en el archivo y las coloca en el objeto JSON
+    for (let i = 1; i <= numero_materias; i++) {
+      const materia = contenido_l[i].trim().split(',');
+      materiasJSON.materias.push({ Nombre: materia[0], Cupos: parseInt(materia[1]) });
+    }
+
+    const numero_estudiantes = contenido_l[numero_materias + 1].trim();
+    const elementos = numero_estudiantes.split(',');
+    let index = 1;
+    let index_materias = 0;
+    let posicion_a = numero_materias + 2;
+
+    // Objeto JSON para almacenar los estudiantes
+    const estudiantesJSON = {
+      numero_estudiantes: numero_estudiantes,
+      estudiantes: []
+    };
+
+    while (true) {
+      if (index > numero_estudiantes) {
+        break;
+      }
+      let estudiante = contenido_l[posicion_a].trim().split(',');
+      let numero_materias = parseInt(estudiante[1]);
+      let materias_estudiante = [];
+      for (let i = posicion_a + 1; i <= numero_materias + posicion_a; i++) {
+        const materia_estudiante = contenido_l[i].trim().split(',');
+        materias_estudiante.push({ Nombre_materia: materia_estudiante[0], Prioridad: parseInt(materia_estudiante[1]) });
+      }
+      estudiantesJSON.estudiantes.push({ Nombre: estudiante[0], Numero_materias: numero_materias, Materias_estudiante: materias_estudiante });
+      posicion_a = numero_materias + posicion_a + 1;
+      materias_estudiante = [];
+      index += 1;
+    }
+    console.log( materiasJSON)
+    console.log( 'hola')
+
+
+    // Devuelve los objetos JSON con los datos procesados
+    return { materias: materiasJSON, estudiantes: estudiantesJSON };
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.log('El archivo no se encontró.');
+    } else {
+      console.log('Ocurrió un error: ' + error.message);
+    }
+    return null;
+  }
+}
+
+module.exports = { readData };
--- Programación Ingenua/Programacion_ingenua.js
+++ Programación Ingenua/Programacion_ingenua.js
@@ -1,38 +1,9 @@
-// try:
-//     with open("input.txt", 'r') as archivo:
-//         contenido_l = archivo.readlines()
-//         numero_materias = int(contenido_l[0].strip())#Guarda el numero de materias están disponibles       
-//         materias = []
-//         for i in range(1,numero_materias+1):
-//           materia = contenido_l[i].strip().split(",")
-//           materias.append({"Nombre":materia[0],"Cupos":int(materia[1])}) 
-//         numero_estudiantes = contenido_l[numero_materias+1].strip()  # Obtiene la línea y elimina los espacios en blanco alrededor
-//         elementos = numero_estudiantes.split(',') # Divide la línea en elementos utilizando la coma como delimitador
-//         print(materias)   
-// except FileNotFoundError:
-//     print(f"El archivo no se encontró.")
-// except Exception as e:
-//     print(f"Ocurrió un error: {str(e)}")
-const fs = require('fs');
+const { readData } = require("../Middleware/Reader.js");
 
-try {
-    const contenido = fs.readFileSync('input.txt', 'utf-8');
-    const contenido_l = contenido.split('\n');
-    const numero_materias = parseInt(contenido_l[0].trim());
-    const materias = [];
-
-    for (let i = 1; i <= numero_materias; i++) {
-        const materia = contenido_l[i].trim().split(',');
-        materias.push({ Nombre: materia[0], Cupos: parseInt(materia[1]) });
-    }
-
-    const numero_estudiantes = contenido_l[numero_materias + 1].trim();
-    const elementos = numero_estudiantes.split(',');
-    console.log(materias);
-} catch (error) {
-    if (error.code === 'ENOENT') {
-        console.log('El archivo no se encontró.');
-    } else {
-        console.log('Ocurrió un error: ' + error.message);
-    }
+const data = readData();
+if (data) {
+  const materiasJSON = data.materias;
+  const estudiantesJSON = data.estudiantes;
+  console.log(materiasJSON);
+  console.log(estudiantesJSON);
 }
